c337aa478f9c0a8c5382eaf27ac6293b4b84efa9
David Preciado <almostvenice@gmail.com>
2022-06-11 10:19:11 -0700

    Header/List/Footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Header/>
+      <CampsitesList/>
+      <Footer/>
     </div>
   );
 }
