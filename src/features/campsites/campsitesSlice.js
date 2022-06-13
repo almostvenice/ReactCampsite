268fab72b1c7531694961865dd1cdb9781d1b24f
David Preciado <almostvenice@gmail.com>
2022-06-13 14:08:45 -0700

    useState on Details

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,14 @@
+import { scryRenderedDOMComponentsWithClass } from 'react-dom/test-utils';
+import { CAMPSITES } from './../../app/shared/CAMPSITES';
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+}
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())]
+// }
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find(campsite => campsite.id === id);
+}
